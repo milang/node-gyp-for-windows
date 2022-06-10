62a6d7e12ca32103ade706ad1dd718c96445e98a
Milan Gardian <mgardian@gmail.com>
2022-06-10 16:09:11 -0600

    Add build script

---
+++ Build.ps1
@@ -0,0 +1,21 @@
+﻿#Requires -version 7
+
+# GitHub Actions workflow commands:
+# https://pakstech.com/blog/github-actions-workflow-commands/
+# https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions
+
+$ErrorActionPreference = "Stop"
+$homeDir = Get-Location
+
+# copy binaries
+Write-Host "::group::Copy binaries"
+$distDir = (New-Item -Force -ItemType Directory $homeDir/app).FullName
+Set-Location $distDir
+yarn init --yes
+yarn add @devcontainers/cli
+Write-Host "::endgroup::"
+
+# package binaries
+Write-Host "::group::Package binaries"
+7z a -r -mx $homeDir/node-gyp-for-windows.7z $distDir/node_modules/*
+Write-Host "::endgroup::"
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# node-gyp-for-windows
+# node-gyp for Windows binaries
+
+[![Build](https://github.com/milang/node-gyp-for-windows/actions/workflows/build.yml/badge.svg)](https://github.com/milang/node-gyp-for-windows/actions/workflows/build.yml)
+
+Use GitHub Actions VM to build node-gyp binaries by installing NPM/yarn packages.
+
+The latest release is currently N/A.
