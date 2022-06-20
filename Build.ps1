be758668c758d8ed63bcb030af67d89ab4c8eff7
Milan Gardian <mgardian@gmail.com>
2022-06-20 13:35:37 -0600

    WIP

--- Build.ps1
+++ Build.ps1
@@ -11,11 +11,18 @@ $homeDir = Get-Location
 Write-Host "::group::Copy binaries"
 $distDir = (New-Item -Force -ItemType Directory $homeDir/app).FullName
 Set-Location $distDir
+
+npm config ls
+Get-ChildItem env:
+Get-ChildItem "C:\Program Files\Microsoft Visual Studio" -Depth 3 | ForEach-Object{ $_.FullName }
+Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio" -Depth 3 | ForEach-Object{ $_.FullName }
+
 yarn init --yes
-yarn add @devcontainers/cli
+# yarn add @devcontainers/cli
 Write-Host "::endgroup::"
 
 # package binaries
 Write-Host "::group::Package binaries"
-7z a -r -mx $homeDir/node-gyp-for-windows.7z $distDir/node_modules/*
+#7z a -r -mx $homeDir/node-gyp-for-windows.7z $distDir/node_modules/*
+7z a -r -mx $homeDir/node-gyp-for-windows.7z $distDir/*
 Write-Host "::endgroup::"
